1b989d33f0d3ab2376aa3aabd9d80f33bf0de075
PawelTkocz <pawel.tkocz@vp.pl>
2024-04-30 12:51:25 +0200

    Car - code cleaning

--- Car.py
+++ Car.py
@@ -1,10 +1,12 @@
 import CarDrafter
 from Wheels import Wheels
+from CarDrafter import CarDrafter
 from Geometry import TurnDir, Vector, Point, Rectangle
 import math
 
 class Car:
-    """Class simulating functioning of car in Cartesian coordinate system"""
+    """Class representing a car in Cartesian coordinate system"""
+
     turning_speed = 0.05
     acceleration = 0.1
     max_speed = 10
@@ -14,16 +16,17 @@ class Car:
     def __init__(self, width, length, front_left_pos, direction, wheels_max_turn, *, color):
         self.width = width
         self.length = length
+
         if direction.x == 0 and direction.y == 0:
-            self.direction = Vector(Point(0, 0), Point(1, 0))
+            self.direction = Vector(1, 0)
         else:
-            self.direction = Vector(Point(0, 0), Point(direction.x, direction.y)).normalize(True)
+            self.direction = Vector(direction.x, direction.y).normalize(False)
 
         self.rect = Rectangle(front_left_pos, width, length, self.direction)
-
         self.wheels = Wheels(wheels_max_turn)
         self.vel = 0
-        self.car_drafter = CarDrafter.CarDrafter(width, length, color)
+        self.car_drafter = CarDrafter(width, length, color)
+
 
     def speed_up_front(self, limit=None):
         if self.vel >= 0:
@@ -84,8 +87,8 @@ class Car:
         return [self.rect.rear_left, self.rect.rear_right, self.rect.front_right, self.rect.front_left]
 
     def draw(self, screen):
-        crnrs = self.get_corners_list()
-        self.car_drafter.draw(crnrs, self.wheels.cur_angle(), self.cur_turn_side(), screen)
+        corners = self.get_corners_list()
+        self.car_drafter.draw(corners, self.wheels.cur_angle(), self.cur_turn_side(), screen)
 
     def cur_movement_vector(self):
         mov_dir = self.direction.copy()
@@ -96,6 +99,7 @@ class Car:
     def move(self):
         if self.vel == 0:
             return
+        
         front_movement_vec = self.cur_movement_vector()
         rear_vel =  (self.length
                      - math.sqrt(self.length**2 - self.vel**2 * self.wheels.sin_cur_angle()**2)
